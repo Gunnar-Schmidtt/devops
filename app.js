fefe318c1f9630689705f99399a50b2ab9844af8
Schmidt <gschmidt1@una.edu>
2026-01-13 13:06:16 -0600

    Express server

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello Express')
+})
+
+//start the server
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
