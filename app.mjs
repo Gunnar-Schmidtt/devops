7eab9ed3d125b2e80dee8bab2d093cc7d8c4bf79
Schmidt <gschmidt1@una.edu>
2026-01-22 13:45:48 -0600

    update

--- app.mjs
+++ app.mjs
@@ -1,10 +1,30 @@
-import express from 'express'
+//app.mjs
+//we are in ES6, use this
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
-const app = express()
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
+app.use(express.json()); 
+
+// middlewares aka endpoints aka 'get to slash' {http verb} to slash {you name ur endpoint}
 app.get('/', (req, res) => {
-  res.send('Hello Express')
+  res.sendFile(join(__dirname, 'public', 'index.html'));
 })
+
+app.get('/api/json', (req, res) =>{
+  const myVar = 'Hello from server!';
+  res.json({ myVar });
+})
+
+app.get('/api/query', (req, res) => {
+
+  console.log("client request with query param:", req.query.name); 
+  res.json({"name": req.query.name});
+});
 
 //start the server
 app.listen(3000, () => {
